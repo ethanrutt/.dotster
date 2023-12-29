c00f7fa1fa95bad2928df81fe7130b1cef7042e6
Ethan Rutt <ethan.rutt2032@gmail.com>
2023-12-29 17:21:31 -0600

    added linking script

---
+++ link.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# cmd to get rid of all symbolic links in home directory
+# find ~ -maxdepth 1 -type l -delete
+
+directory="$(pwd)/ubuntu"
+echo "$directory"
+
+for file in $directory/.*; do
+	stripped_file="$(basename $file)"
+	if [ $stripped_file != "." ] && [ $stripped_file != ".." ]; then
+		ln -s "$file" ~
+	fi
+done
+
