a308c4e05d4482e8e8e66fab5d90ffbce80e63f0
Ethan Rutt <ethan.rutt2032@gmail.com>
2024-01-25 13:30:46 -0600

    modified install and link scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cwd="$(pwd)"
+echo "$cwd"
+
+echo "switching to ~ dir and installing common packages"
 cd ~
 sudo apt update
 sudo apt install git
@@ -10,7 +14,12 @@ sudo apt install tmux
 sudo apt install oh-my-posh
 sudo apt install fzf
 
-git clone https://github.com/JanDeDobbeleer/oh-my-posh.git
+echo "cloning oh-my-posh"
 echo "!!!"
 echo "remember to move themes out of oh-my-posh, set themes dir in bashrc, and you can delete the rest"
 echo "!!!"
+git clone https://github.com/JanDeDobbeleer/oh-my-posh.git
+
+echo "switching back to .dotster and linking dotfiles"
+cd "$cwd"
+./link.sh
--- link.sh
+++ link.sh
@@ -8,10 +8,17 @@ echo "$directory"
 
 for file in $directory/.*; do
 	stripped_file="$(basename $file)"
+	echo "$stripped_file"
 
 	# don't want to link . and .. directories
-	if [ $stripped_file != "." ] && [ $stripped_file != ".." ]; then
+	if [ $stripped_file == "." ] || [ $stripped_file == ".." ]; then
+		continue
+	elif [ -e "$HOME/$stripped_file" ]; then
+		rm "$HOME/$stripped_file"
+		ln -s "$file" ~
+	else
 		ln -s "$file" ~
 	fi
+
 done
 
